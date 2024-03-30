3ba5808c2d340cd1eece899b07b1f2c5cfbcf902
Ayyoob Ajward <ajwardayyoob@gmail.com>
2024-03-31 00:27:43 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/HeroSection/HeroSection";
+
+export default function App() {
+  return (
+    <div className="max-h-full">
+      <Navbar />
+      <Hero />
+    </div>
+  )
+}
---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,32 @@
+import { TypeAnimation } from 'react-type-animation';
+
+function HeroSection() {
+  return (
+    <div className="min-h-[600px] w-full flex justify-between items-center px-36">
+      <div className="max-w-full">
+      <h1 className="text-5xl font-bold text-blue-600">I'am Ayyoob Ajward</h1>
+      <TypeAnimation
+      sequence={[
+        'You call me a Full Stack Developer',
+        1000,
+        'You call me a (Alim) Islamic Scholar',
+        1000,
+        'You call me a Web Developer',
+        1000,
+        'You call me a Digital Marketer',
+        1000
+      ]}
+      wrapper="span"
+      speed={50}
+      style={{ fontSize: '1.6em', display: 'inline-block' }}
+      repeat={Infinity}
+    />
+      </div>
+      <div className="max-w-full">
+        
+      </div>
+  </div>
+  )
+}
+
+export default HeroSection;
