c72587216a3c2cdcfdebeaaec1736a5461f63d4c
Ayyoob Ajward <ajwardayyoob@gmail.com>
2024-03-31 20:19:27 +0530

    responsive

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <div className='customSection'>
         <div>
-            <h1 className='pb-10 font-bold text-center text-blue-600 text-7xl'>Contact</h1>
+            <h1 className='pt-24 pb-10 font-bold text-center text-blue-600 text-7xl md:pt-0'>Contact</h1>
         </div>
         <div className='flex flex-col items-center'>
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,14 +2,14 @@
 
 function Projects() {
   return (
-    <div className="flex flex-col items-center justify-start bg-gray-200 customSection">
+    <div className="flex flex-col items-center justify-start pb-24 bg-gray-200 customSection">
       <div>
-        <h1 className="pt-10 pb-10 font-bold text-center text-blue-600 text-7xl">
+        <h1 className="pt-24 pb-24 font-bold text-center text-blue-600 md:pt-0 text-7xl">
           Projects
         </h1>
-        <div className="grid grid-cols-1 gap-5 md:grid-cols-3 sm:grid-cols-2">
-          <div className="rounded-md drop-shadow-sm min-w-[400px] md:w-min-[300px]  lg:w-min-[400px]  bg-gray-400 min-h-80"></div>
-          <div className="rounded-md drop-shadow-sm min-w-[400px] md:w-min-[300px]  lg:w-min-[400px]   bg-gray-400 min-h-80"></div>
+        <div className="grid grid-cols-1 gap-5 px-10 md:grid-cols-3 sm:grid-cols-2">
+          <div className="rounded-md drop-shadow-sm min-w-[400px] md:w-min-[300px]  lg:w-min-[400px] bg-gray-400 min-h-80"></div>
+          <div className="rounded-md drop-shadow-sm min-w-[400px] md:w-min-[300px]  lg:w-min-[400px] bg-gray-400 min-h-80"></div>
           <div className="rounded-md drop-shadow-sm min-w-[400px] md:w-min-[300px]  lg:w-min-[400px] bg-gray-400 min-h-80"></div>
           <div className="rounded-md drop-shadow-sm min-w-[400px] md:w-min-[300px]  lg:w-min-[400px] bg-gray-400 min-h-80"></div>
           <div className="rounded-md drop-shadow-sm min-w-[400px] md:w-min-[300px]  lg:w-min-[400px] bg-gray-400 min-h-80"></div>
