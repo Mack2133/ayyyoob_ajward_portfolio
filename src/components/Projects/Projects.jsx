17e886efebc3ef5b7289194b81019c999fc29740
Ayyoob Ajward <ajwardayyoob@gmail.com>
2024-03-31 23:35:13 +0530

    responsive

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -58,7 +58,7 @@ function Projects() {
         <h1 className="pt-24 pb-24 font-bold text-center text-blue-600 md:pt-0 text-7xl">
           Projects
         </h1>
-        <div className="z-0 grid grid-cols-1 gap-5 md:grid-cols-3 sm:grid-cols-2 sm:gap-5">
+        <div className="z-0 grid grid-cols-1 gap-5 px-10 md:grid-cols-3 sm:grid-cols-2 sm:gap-5 md:px-0">
           {projects.map((project) => (
             <CustomCard
               key={project.id}
